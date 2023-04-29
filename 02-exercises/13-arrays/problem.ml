9d2ad36ae21dacad72d56f6aa4530a83cc56d9ac
Mirlan Ipasov <mipasov@gmail.com>
2023-04-30 00:24:59 +0600

    arrays, variants and options

--- 02-exercises/13-arrays/problem.ml
+++ 02-exercises/13-arrays/problem.ml
@@ -54,7 +54,9 @@ let () =
 
    Let's implement a function [double] using [Array.iteri], which takes an [int
    array] and doubles each element of the array in place. *)
-let double array : unit = failwith "For you to implement"
+
+
+let double array : unit = Array.iteri (fun i x -> array.(i) <- x * 2) array
 
 let%test "Testing double..." = 
   let array = [| 1; 1; 1 |] in
@@ -72,7 +74,10 @@ let%test "Testing double..." =
 
 (* Write a function that takes an [int array] and a list of indicies and
    doubles each of the elements at the specified indices. *)
-let double_selectively array indices : unit = failwith "For you to implement"
+
+
+let double_selectively array indices : unit =
+  Array.iteri (fun i x -> Array.iter (fun y -> if i == y then array.(i) <- 2 * x) indices) array;;
 
 let%test "Testing double_selectively..." = 
   let array = [| 1; 1; 1 |] in
--- 02-exercises/15-options/problem.ml
+++ 02-exercises/15-options/problem.ml
@@ -31,7 +31,10 @@ let%test _ =
 (* Implement the function [safe_divide ~dividend ~divisor], which takes two
    [int]s and returns an [int option]. It should return [None] if [divisor = 0],
    and otherwise return [Some x] where [x] is the division result *)
-let safe_divide ~dividend ~divisor = failwith "For you to implement"
+let safe_divide ~dividend ~divisor = 
+  match (dividend, divisor) with
+  | (_, 0) -> None
+  | (_, _) -> Some (dividend / divisor)
 
 let%test "Testing safe_divide..." =
   match (safe_divide ~dividend:3 ~divisor:2) with
@@ -47,7 +50,11 @@ let%test "Testing safe_divide..." =
    returns a [string option] that is:
    - [Some x], where x is the concatenation of the two strings, if they both exist
    - [None] if either of the strings is [None]  *)
-let option_concatenate string1 string2 = failwith "For you to implement" 
+let option_concatenate string1 string2 = 
+  match (string1, string2) with
+  | (_, None) -> None
+  | (None, _) -> None
+  | (Some s1, Some s2) -> Some (s1 ^ s2) 
 
 let%test "Testing option_concatenate..." =
   match option_concatenate (Some "hello") (Some "world") with
@@ -119,8 +126,8 @@ let labeled_concatenate ?(separator = "")  ~string1 ~string2 =
   string1 ^ separator ^ string2
 
 (* Try uncommenting this code. What is the compile error? *)
-(* let () = 
- *   assert (String.(=) "hi" (labeled_concatenate ~string1:"h" ~string2:"i")) *)
+let () = 
+  assert (String.(=) "hi" (labeled_concatenate ?separator:None ~string1:"h" ~string2:"i"))
 
 (* This is because optional arguments can only be safely omitted if there's an
    unlabeled, non-optional argument after the optional argument in the function
